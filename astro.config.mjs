892e43807d5ddf29034aaf9bd6443e026909d24e
中村祐馬のPC <yumanakamurayuma@nakamurayuubanopishinoMacBook-Air.local>
2024-10-28 15:20:59 +0900

    コメントの修正

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  // `astro dev` と `astro preview`実行時の開発用サーバー設定
+  // `make dev` と `make preview`実行時のサーバー設定
   server: {
     host: "127.0.0.1",
   },
