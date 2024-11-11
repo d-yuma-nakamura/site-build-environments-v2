45450fefc3c09bba6544ccf1b74343c4d30cbf55
中村祐馬のPC <yumanakamurayuma@nakamurayuubanopishinoMacBook-Air.local>
2024-11-11 16:59:50 +0900

    サンプルサイトの設定

---
+++ sites/directus-site/src/lib/directus.ts
@@ -0,0 +1,18 @@
+import { createDirectus, rest } from "@directus/sdk";
+
+type News = {
+  title: string;
+  publish_date: Date;
+  description: string;
+  subtitle: string;
+  content: string;
+  tags: string;
+};
+
+type Schema = {
+  news: News;
+};
+
+const directus = createDirectus<Schema>("http://localhost:8055/").with(rest());
+
+export default directus;
